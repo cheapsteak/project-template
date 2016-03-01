7051c4361bed53bb631e702130a891040043cb07
Benson Wong <wongbsn@gmail.com>
2016-03-01 14:18:08 -0500

    Refactored photo-essay

--- src/common/components/photo-essay/photo-essay-actions.js
+++ src/common/components/photo-essay/photo-essay-actions.js
@@ -1,21 +1,54 @@
+import Promise from 'bluebird';
+import dataModel from '../../data/photo-essays.js';
+
 export const PREV_PHOTO = 'PREV_PHOTO';
 export const NEXT_PHOTO = 'NEXT_PHOTO';
-export const TOGGLE_FULLSCREEN = 'TOGGLE_FULLSCREEN';
+export const REQUEST_ESSAY = 'REQUEST_ESSAY';
+export const RECIEVE_ESSAY = 'RECIEVE_ESSAY';
 
-export function setPrevPhoto() {
+
+export function setPrevPhoto(id) {
   return {
-    type: PREV_PHOTO
+    type: PREV_PHOTO,
+    id
   };
 }
 
-export function setNextPhoto() {
+export function setNextPhoto(id) {
   return {
-    type: NEXT_PHOTO
+    type: NEXT_PHOTO,
+    id
   };
 }
 
-export function toggleFullscreen() {
+export function requestPhotoEssay(id) {    
   return {
-    type: TOGGLE_FULLSCREEN
-  };
+    type: REQUEST_ESSAY,
+    id
+  }
+}
+
+export function recievePhotoEssay(id, data) {
+  return {
+    type: RECIEVE_ESSAY,
+    id,
+    data
+  }
+}
+
+export function loadPhotoEssay(id) {
+  return (dispatch) => {
+    dispatch(requestPhotoEssay(id));
+
+    return new Promise((resolve, reject) => {
+      if(!dataModel[id]) {
+        reject(id);
+      }
+
+      resolve(dataModel[id]);
+    })
+    .then((data) =>{
+      dispatch(recievePhotoEssay(id, data));
+    })
+  }
 }
--- src/common/components/photo-essay/photo-essay.jsx
+++ src/common/components/photo-essay/photo-essay.jsx
@@ -3,88 +3,69 @@ import { findDOMNode } from 'react-dom';
 import BackButtonSvg from '../../../assets/photo-essay-prev-button.svg';
 import NextButtonSvg from '../../../assets/photo-essay-next-button.svg';
 import FullscreenButtonSvg from '../../../assets/photo-essay-fullscreen-button.svg';
+import * as actionCreators from './photo-essay-actions.js';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import store from '../../store.js';
 
-export default class PhotoEssay extends React.Component {
+@connect(state => ({ photoEssays: state.photoEssays}) )
+class PhotoEssay extends React.Component {
   constructor(props) {
     super(props)
+    this.boundActionCreators = bindActionCreators(actionCreators, props.dispatch);
 
-    this.state = {
-      current: props.index
+    if(!props.photoEssays[props.model]) {
+      props.dispatch( actionCreators.loadPhotoEssay(props.model) )
     }
   }
 
-  componentWillReceiveProps(nextProps) {        
-    this.setState({ current: nextProps.index });
-    this.setFullscreen(nextProps.isFullscreen);
-  }
-
-  setFullscreen = (isFullscreen) => {
-
-    // NOTE: This is not the actual implementation
-    const el = findDOMNode(this);
-
-    if(isFullscreen) {
-      if (el.requestFullscreen) {
-        el.requestFullscreen();
-      } else if (el.msRequestFullscreen) {
-        el.msRequestFullscreen();
-      } else if (el.mozRequestFullScreen) {
-        el.mozRequestFullScreen();
-      } else if (el.webkitRequestFullscreen) {
-        el.webkitRequestFullscreen();
-      }
-    } else {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-      } else if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-      } else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-      } else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-      }
+  setFullBrowser = (isFullBrowser) => {
+    if(isFullBrowser) {
+      this.props.toggleFullBrowser(PhotoEssay, this.props);
     }
   };
 
   handlePrevClick = () => {
-    this.props.prev()
-
-    // this.setState({ 
-    //   current: this.state.current > 0 
-    //     ? (this.state.current - 1) % this.props.photos.length
-    //     : this.props.photos.length - 1
-    // });
+    this.boundActionCreators.setPrevPhoto(this.props.model);
   };
 
   handleNextClick = () => {
-    this.props.next()
-    // this.setState({ current: (this.state.current + 1) % this.props.photos.length })
+    this.boundActionCreators.setNextPhoto(this.props.model);
   };
 
-  handleFullscreenClick = () => {
-    this.props.toggleFullscreen();
+  handleFullBrowserClick = () => {
+    this.props.toggleFullBrowser(PhotoEssay, this.props);
   };
 
   render () {
-    const { style, photos } = this.props;
-    const index = this.state.current;
+    const { model, style, photoEssays } = this.props;
+    const photoEssay = photoEssays[model];
+    let el = <div />;
 
-    return (
-      <div className="photo-essay" style={style}>
-        <div className="image-wrapper">
-          <img src={photos[index].image} />
+    if(photoEssay && !photoEssay.isLoading) {
+      const photo = photoEssay.photos[photoEssay.index];
+      
+      el = (
+        <div className="photo-essay" style={style}>
+          <div className="image-wrapper">
+            <img src={photo.image} />
+          </div>
+          <div className="photo-description">
+            <h3>About this picture</h3>
+            <p>{ photo.description }</p>
+          </div>
+          <div className="photo-controls">
+            <div className="button back-button" onClick={this.handlePrevClick} dangerouslySetInnerHTML={{ __html: BackButtonSvg }}></div>
+            <div className="button next-button" onClick={this.handleNextClick} dangerouslySetInnerHTML={{ __html: NextButtonSvg }}></div>
+            <div className="button fullscreen-button" onClick={this.handleFullBrowserClick} dangerouslySetInnerHTML={{ __html: FullscreenButtonSvg }}></div>
+            <div className="page-display">{`${photoEssay.index + 1} of ${photoEssay.photos.length}`}</div>
+          </div>
         </div>
-        <div className="photo-description">
-          <h3>About this picture</h3>
-          <p>{ photos[index].description }</p>
-        </div>
-        <div className="photo-controls">
-          <div className="button back-button" onClick={this.handlePrevClick} dangerouslySetInnerHTML={{ __html: BackButtonSvg }}></div>
-          <div className="button next-button" onClick={this.handleNextClick} dangerouslySetInnerHTML={{ __html: NextButtonSvg }}></div>
-          <div className="button fullscreen-button" onClick={this.handleFullscreenClick} dangerouslySetInnerHTML={{ __html: FullscreenButtonSvg }}></div>
-          <div className="page-display">{`${index + 1} of ${this.props.photos.length}`}</div>
-        </div>
-      </div>
-    )
+      );
+    }
+
+    return el;
   }
 }
+
+export default PhotoEssay
