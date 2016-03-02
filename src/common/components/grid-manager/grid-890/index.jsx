9419fc311525d0cbbefcba7f668d400a3add17de
vadim <vadim@jam3.com>
2016-03-02 17:56:47 -0500

    dom clean-up

--- src/common/components/grid-manager/grid-890/index.jsx
+++ src/common/components/grid-manager/grid-890/index.jsx
@@ -60,60 +60,47 @@ export default class Grid extends React.Component {
     return (
       <div className={`grid grid-890 ${this.state.status}`}>
 
-        <div className={`grid-row`}>
-          <div className={`grid-item first-row width-2 right-padding`} style={{height: this.state.baseHeight}}>
-            <div className={`nested-bar-width-100-height-33 red`}></div>
-            <div className={`nested-cube-height-66 top-margin grey`}></div>
-          </div>
-          <div className={`grid-item first-row width-3 height-full orange`}
-               style={{height: this.state.baseHeight}}></div>
+        <div className={`grid-item first-row width-2 right-padding`} style={{height: this.state.baseHeight}}>
+          <div className={`nested-bar-width-100-height-33 red`}></div>
+          <div className={`nested-cube-height-66 top-margin grey`}></div>
+        </div>
+        <div className={`grid-item first-row width-3 height-full orange`} style={{height: this.state.baseHeight}}></div>
+
+        <div className={`grid-item width-3`} style={{height: this.state.twoThirdHeight}}>
+          <div className={`full-size light-blue`}></div>
+        </div>
+        <div className={`grid-item width-2 left-padding`} style={{height: this.state.twoThirdHeight}}>
+          <div className={`nested-cube-50 red`}></div>
+          <div className={`nested-cube-50 left-margin red`}></div>
+          <div className={`nested-bar-width-100-height-50 top-margin red`}></div>
         </div>
 
-        <div className={`grid-row`}>
-          <div className={`grid-item width-3`} style={{height: this.state.twoThirdHeight}}>
-            <div className={`full-size light-blue`}></div>
-          </div>
-          <div className={`grid-item width-2 left-padding`} style={{height: this.state.twoThirdHeight}}>
-            <div className={`nested-cube-50 red`}></div>
-            <div className={`nested-cube-50 left-margin red`}></div>
-            <div className={`nested-bar-width-100-height-50 top-margin red`}></div>
-          </div>
+        <div className={`grid-item width-2 right-padding`} style={{height: this.state.baseHeight}}>
+          <div className={`nested-bar-width-100-height-33 dark-blue`}></div>
+          <div className={`nested-bar-width-50-height-66 top-margin red`}></div>
+          <div className={`nested-cube-33 top-margin left-margin dark-blue`}></div>
+          <div className={`nested-cube-33 top-margin left-margin dark-blue`}></div>
+        </div>
+        <div className={`grid-item width-3 light-blue`} style={{height: this.state.baseHeight}}></div>
+
+        <div className={`grid-item width-3 height-2 light-blue`} style={{height: this.state.twoThirdHeight}}></div>
+        <div className={`grid-item width-2 height-2 left-padding `} style={{height: this.state.twoThirdHeight}}>
+          <div className={`full-size grey`}></div>
         </div>
 
-        <div className={`grid-row`}>
-          <div className={`grid-item width-2 right-padding`} style={{height: this.state.baseHeight}}>
-            <div className={`nested-bar-width-100-height-33 dark-blue`}></div>
-            <div className={`nested-bar-width-50-height-66 top-margin red`}></div>
-            <div className={`nested-cube-33 top-margin left-margin dark-blue`}></div>
-            <div className={`nested-cube-33 top-margin left-margin dark-blue`}></div>
-          </div>
-          <div className={`grid-item width-3 light-blue`} style={{height: this.state.baseHeight}}></div>
+        <div className={`grid-item width-3 grey`} style={{height: this.state.baseHeight}}></div>
+        <div className={`grid-item width-2 left-padding`} style={{height: this.state.twoThirdHeight}}>
+          <div className={`nested-cube-50 dark-blue`}></div>
+          <div className={`nested-cube-50 left-margin dark-blue`}></div>
+          <div className={`nested-bar-width-100-height-50 top-margin red`}></div>
         </div>
 
-        <div className={`grid-row`}>
-          <div className={`grid-item width-3 height-2 light-blue`} style={{height: this.state.twoThirdHeight}}></div>
-          <div className={`grid-item width-2 height-2 left-padding `} style={{height: this.state.twoThirdHeight}}>
-            <div className={`full-size grey`}></div>
-          </div>
+        <div className={`grid-item width-3`} style={{height: this.state.twoThirdHeight}}>
+          <div className={`nested-bar-width-33-height-100 red`}></div>
+          <div className={`nested-cube-width-66 left-margin grey`}></div>
         </div>
-
-        <div className={`grid-row`}>
-          <div className={`grid-item width-3 grey`} style={{height: this.state.baseHeight}}></div>
-          <div className={`grid-item width-2 left-padding`} style={{height: this.state.twoThirdHeight}}>
-            <div className={`nested-cube-50 dark-blue`}></div>
-            <div className={`nested-cube-50 left-margin dark-blue`}></div>
-            <div className={`nested-bar-width-100-height-50 top-margin red`}></div>
-          </div>
-        </div>
-
-        <div className={`grid-row`}>
-          <div className={`grid-item width-3`} style={{height: this.state.twoThirdHeight}}>
-            <div className={`nested-bar-width-33-height-100 red`}></div>
-            <div className={`nested-cube-width-66 left-margin grey`}></div>
-          </div>
-          <div className={`grid-item width-2 left-padding`} style={{height: this.state.baseHeight}}>
-            <div className={`full-size grey`}></div>
-          </div>
+        <div className={`grid-item width-2 left-padding`} style={{height: this.state.baseHeight}}>
+          <div className={`full-size grey`}></div>
         </div>
 
       </div>
