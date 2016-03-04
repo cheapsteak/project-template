b4e4b18a702907f5892b56d93b7b1262586b1a55
vadim <vadim@jam3.com>
2016-03-04 10:05:58 -0500

    detector

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 require('babel-polyfill');
 require('./globals.js');
 import stats from 'common/utils/stats';
+import detect from 'common/utils/detect';
 
 import { render } from 'react-dom';
 import domready from 'domready';
@@ -11,6 +12,9 @@ import fastclick from 'fastclick';
 ga('create', global.CONFIG.gaID, 'auto');
 
 domready(function () {
+  var page = document.getElementsByTagName('html')[0];
+  page.setAttribute('class', detect.className);
+
   fastclick(document.body);
   render(router, document.getElementById('container'));
 });
