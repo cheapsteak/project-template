f4fe379a3476e6a4e000dd03732e437717bfcefc
vadim <vadim@jam3.com>
2016-03-07 17:58:39 -0500

    updated kid path

--- src/common/data/grid.js
+++ src/common/data/grid.js
@@ -2,47 +2,47 @@ const data = {
   welcome: {
     title: 'welcome',
     subtitle: 'chapter',
-    image: '../images/test-kid.png'
+    image: '../images/kid-test.png'
   },
   science: {
     title: 'science',
     subtitle: 'chapter',
-    image: '../images/test-kid.png'
+    image: '../images/kid-test.png'
   },
   literacy: {
     title: 'literacy & writing',
     subtitle: 'chapter',
-    image: '../images/test-kid.png'
+    image: '../images/kid-test.png'
   },
   math: {
     title: 'math',
     subtitle: 'chapter',
-    image: '../images/test-kid.png'
+    image: '../images/kid-test.png'
   },
   electives: {
     title: 'electives',
     subtitle: 'chapter',
-    image: '../images/test-kid.png'
+    image: '../images/kid-test.png'
   },
   computer: {
     title: 'computer science',
     subtitle: 'chapter',
-    image: '../images/test-kid.png'
+    image: '../images/kid-test.png'
   },
   investment: {
     title: 'parental investment',
     subtitle: 'chapter',
-    image: '../images/test-kid.png'
+    image: '../images/kid-test.png'
   },
   development: {
     title: 'character development',
     subtitle: 'chapter',
-    image: '../images/test-kid.png'
+    image: '../images/kid-test.png'
   },
   history: {
     title: 'history',
     subtitle: 'chapter',
-    image: '../images/test-kid.png'
+    image: '../images/kid-test.png'
   }
 };
 
