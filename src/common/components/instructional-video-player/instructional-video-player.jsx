2b289b493863b3499f5e5a9cee7154741e610f18
Benson Wong <wongbsn@gmail.com>
2016-03-08 17:05:07 -0500

    Refactored full browser wrapper to expose/link react transition hooks

--- src/common/components/instructional-video-player/instructional-video-player.jsx
+++ src/common/components/instructional-video-player/instructional-video-player.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { findDOMNode } from 'react-dom';
 import Timeline from 'common/components/timeline/timeline';
 import PlayButtonSvg from '../../../assets/video-play-button.svg';
 import FullBrowserButtonSvg from '../../../assets/photo-essay-fullscreen-button.svg';
@@ -9,9 +10,7 @@ export default class InstructionalVideoPlayer extends React.Component {
     super(props);
 
     this.state = {
-      time: 0,
-      isPlaying: false,
-      isVideoLoaded: false
+      isPlaying: false
     };
   }
 
@@ -21,29 +20,67 @@ export default class InstructionalVideoPlayer extends React.Component {
     timeline: React.PropTypes.array
   };
 
+  videoId = 'target-video';
+  cloneId = 'clone-video';
+
+  componentWillEnterFullBrowser = () => {
+    const container = findDOMNode(this);
+    const video = document.querySelector(`#${this.videoId}`);
+    const videoParent = video.parentNode;
+    const clone = video.cloneNode();
+    const isPlaying = !video.paused;
+
+    clone['data-reactid'] = new Date().getTime();
+    clone.id = this.cloneId;
+
+    videoParent.removeChild(video)
+    videoParent.insertBefore(clone, videoParent.firstChild);
+    container.insertBefore(video, container.firstChild);
+
+    this.video = video;
+
+    isPlaying && video.play();
+
+    return Promise.resolve();
+  };
+
+  componentWillLeaveFullBrowser = () => { 
+    const container = findDOMNode(this);
+    const clone = document.querySelector('#clone-video');
+    const cloneParent = clone.parentNode;
+    const video = document.querySelector(`#${this.videoId}`);
+    const isPlaying = !video.paused;
+
+    cloneParent.removeChild(clone)
+    cloneParent.insertBefore(video, cloneParent.firstChild);
+
+    isPlaying && video.play();
+
+    return Promise.resolve();
+  };
+
   changeVideoTime = (time) => {
     this.video.currentTime = time;
   };
 
-  pauseVideo = () => {
-    this.video.pause();
-  };
-
   handleMetadataLoaded = () => {
-    this.setState({isVideoLoaded: true});
-    this.video.currentTime = this.props.startTime;
+    this.video.currentTime = this.props.currentTime;
+    this.props.onVideoMetadataLoaded && this.props.onVideoMetadataLoaded(this.video.duration);
   };
 
-  handleTimeUpdate = (e) => {
-    this.setState({time: e.target.currentTime});
+  handleTimeUpdate = () => {
+    this.props.onVideoTimeChange(this.video.currentTime);
+    this.setState({time: this.video.currentTime});
   };
 
   handleVideoPlayPause = () => {
-    this.state.isPlaying
-      ? this.video.pause()
-      : this.video.play();
-
-    this.setState({isPlaying: !this.state.isPlaying})
+    if(this.props.isPlaying) {
+      this.video.pause();
+      this.props.onVideoPause();
+    } else {
+      this.video.play();
+      this.props.onVideoPlay();
+    }
   };
 
   handlePrevClick = (e) => {
@@ -54,13 +91,9 @@ export default class InstructionalVideoPlayer extends React.Component {
 
   };
 
-  handleFullBrowserClick = (e) => {
-    this.props.onFullBrowserClick && this.props.onFullBrowserClick(this.state.time);
-  };
-
   render() {
-    const { style, modelSlug, basePath } = this.props;
-    const tempPauseStyle = this.state.isPlaying ? {fill: 'black'} : undefined;
+    const { style, modelSlug, basePath, isFullBrowser } = this.props;
+    const tempPauseStyle = this.props.isPlaying ? {fill: 'black'} : undefined;
 
     let route = `${basePath}`;
 
@@ -69,16 +102,21 @@ export default class InstructionalVideoPlayer extends React.Component {
     }
 
     return <div className="instructional-video-player" style={style}>
-      <video
-        ref={(node) => this.video = node }
-        src={this.props.src}
-        preload="metadata"
-        onLoadedMetadata={this.handleMetadataLoaded}
-        onEnded={this.handleVideoPlayPause}
-        onTimeUpdate={this.handleTimeUpdate}
-      >
-      </video>
-      <div className="controls">
+      {
+        !isFullBrowser ? 
+          <video
+            id={this.videoId}
+            preload="metadata"
+            ref={(node) => this.video = node }
+            src={this.props.src}
+            onLoadedMetadata={this.handleMetadataLoaded}
+            onEnded={this.handleVideoPlayPause}
+            onTimeUpdate={this.handleTimeUpdate}
+          >
+          </video>
+          : undefined
+      }
+      <div className="controls" ref="controls">
         <div className="control-group">
           <span
             className="button"
@@ -94,9 +132,15 @@ export default class InstructionalVideoPlayer extends React.Component {
             </span>
           </Link>
         </div>
+        {
+          /* 
+            The duration is put into the store and pass down to the component
+            to account for the work around with moving around the video node 
+          */
+        }
         <Timeline
-          currentTime={this.state.time}
-          duration={ this.state.isVideoLoaded ? this.video.duration : 0 }
+          currentTime={this.props.currentTime || 0}
+          duration={this.props.duration || 0}
           onTimeChange={this.changeVideoTime}
           items={[]}
         />
