95cad7ff8aab842c2883f9bf7cf526234527f802
vadim <vadim@jam3.com>
2016-03-10 13:59:45 -0500

    fixing video container and adding 5 videos

---
+++ src/tests/parallax-video/history.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ParallaxVideo from 'common/components/parallax-video/parallax-video';
+
+export default function () {
+
+  return (
+    <ParallaxVideo
+      bgVideoPath={'../videos/bg-1080.mp4'}
+      fgVideoPath={'../videos/parallax-history.mp4'}
+    >
+      <div className={`parallax-layer`} data-depth="0.3">
+        <div className={`text-container`}>
+          <div className={`title`}>Chapter</div>
+          <div className={`subtitle`}>Computer Science</div>
+          <div className={`description`}>At Success Academy we completely redefined how to teach Science.</div>
+        </div>
+      </div>
+    </ParallaxVideo>
+  )
+}
