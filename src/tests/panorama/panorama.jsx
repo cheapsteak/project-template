b770a5802c98ed61cafc6d93b6df875e6b3e57bc
vadim <vadim@jam3.com>
2016-03-17 12:06:39 -0400

    panorama input

--- src/tests/panorama/panorama.jsx
+++ src/tests/panorama/panorama.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Panorama from 'common/components/panorama/panorama.jsx';
 
-export default function () {
-  return (
-    <div style={{height: 600}}>
-      <Panorama
-        src={'../images/pan-11.jpg'}
-      />
-    </div>
-  )
+export default class PanoramaTest extends React.Component {
+
+  state = {
+    value: '../images/pan-11.jpg'
+  };
+
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  };
+
+  render() {
+    return (
+      <div>
+        <div style={{height: 600}}>
+          <Panorama src={this.state.value}/>
+        </div>
+        <input type="text"
+               onChange={this.handleChange}
+               style={{marginTop: 100, marginLeft: 200,width: 400, height: 50}}
+               placeholder="URL"
+        />
+      </div>
+    )
+  }
 }
