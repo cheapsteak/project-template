1b57bc811abfa6510fa144adb7441ef318aae29e
vadim <vadim@jam3.com>
2016-03-22 10:19:30 -0400

    fixing loader errors

--- src/common/components/panorama/panorama-loader/panorama-loader.jsx
+++ src/common/components/panorama/panorama-loader/panorama-loader.jsx
@@ -2,32 +2,9 @@ import React from 'react';
 import {findDOMNode} from 'react-dom';
 import animate from 'gsap-promise';
 
-export default class PanoramaMenu extends React.Component {
+export default class PanoramaLoader extends React.Component {
 
-  static propTypes = {
-    modes: React.PropTypes.objectOf(React.PropTypes.string).isRequired,
-    currMode: React.PropTypes.string.isRequired,
-    setPanorama: React.PropTypes.func
-  };
-
-  componentWillReceiveProps(newProps) {
-    if (newProps.currMode !== this.currMode) {
-      this.currMode = newProps.currMode;
-
-      switch (newProps.currMode) {
-        case this.props.modes.ENTER_IDLE:
-        case this.props.modes.ENTER_FB:
-          this.animateIn();
-          break;
-        case this.props.modes.LEAVE_IDLE:
-        case this.props.modes.LEAVE_FB:
-          this.animateOut();
-          break;
-        default:
-          break;
-      }
-    }
-  }
+  static propTypes = {};
 
   componentDidMount() {
     this.containerEl = findDOMNode(this);
