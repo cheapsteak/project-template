2f68bfb2de7f4a8cb3836eb58fe7f6bf16ab6e42
Benson Wong <wongbsn@gmail.com>
2016-03-23 10:19:47 -0400

    Removed image for hover cards

--- src/common/data/instructional-videos.js
+++ src/common/data/instructional-videos.js
@@ -7,20 +7,20 @@ export default [
     chapterSlug: 'math',
     src: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4',
     endingCardImage: `${config.ASSET_PATH}/learn-more.jpg`,
-    hoverCardImage: `${config.ASSET_PATH}/grid-hover-card.jpg`
+    // hoverCardImage: `${config.ASSET_PATH}/grid-hover-card.jpg`
   },
   {
     slug: 'science-1',
     chapterSlug: 'science',
     src: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4',
     endingCardImage: `${config.ASSET_PATH}/learn-more.jpg`,
-    hoverCardImage: `${config.ASSET_PATH}/grid-hover-card.jpg`
+    // hoverCardImage: `${config.ASSET_PATH}/grid-hover-card.jpg`
   },
   {
     slug: 'science-2',
     chapterSlug: 'science',
     src: 'http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4',
     endingCardImage: `${config.ASSET_PATH}/learn-more.jpg`,
-    hoverCardImage: `${config.ASSET_PATH}/grid-hover-card.jpg`
+    // hoverCardImage: `${config.ASSET_PATH}/grid-hover-card.jpg`
   }
 ]
