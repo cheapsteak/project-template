3807c903e3d9d5c16d13adbd0c6fcdd6e8b13c52
vadim <vadim@jam3.com>
2016-03-26 17:00:35 -0400

    play button and podcast styles

--- src/tests/podcast/podcast.jsx
+++ src/tests/podcast/podcast.jsx
@@ -4,9 +4,9 @@ import Podcast from 'common/components/podcast/podcast.jsx';
 export default class PodcastTest extends React.Component {
   render() {
     return (
-      <div style={{height: 480}}>
+      <div style={{height: '60vh'}}>
         <Podcast
-          src={`http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4`}
+          src={`https://player.vimeo.com/external/160156123.hd.mp4?s=3a124481f349bd0b62a5731ea4ad1166caf5dd40&profile_id=119`}
         />
       </div>
     )
