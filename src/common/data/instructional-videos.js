dae6a07bb00c795b7be7de82114952472b05914e
Benson Wong <wongbsn@gmail.com>
2016-03-30 18:55:02 -0400

    Fixed slug

--- src/common/data/instructional-videos.js
+++ src/common/data/instructional-videos.js
@@ -3,7 +3,7 @@ import config from '../../../config.js';
 // Order here matters - Chang
 export default [
   {
-    slug: 'math',
+    slug: 'math-1',
     chapterSlug: 'math',
     src: 'https://player.vimeo.com/external/160156123.hd.mp4?s=3a124481f349bd0b62a5731ea4ad1166caf5dd40&profile_id=119',
     endingCardImage: `${config.ASSET_PATH}/learn-more.jpg`,
