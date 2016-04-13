3925358096a521caa27ca2a48010fad13c2ae77c
Chang Wang <garnwraly@gmail.com>
2016-04-13 11:07:22 -0400

    added global.ViniEaseOut

--- src/globals.js
+++ src/globals.js
@@ -3,3 +3,4 @@ global.ASSET_PATH = global.CONFIG.ASSET_PATH;
 
 global.BezierEasing = require('bezier-easing');
 global.audio = require('common/utils/audio');
+global.ViniEaseOut = BezierEasing(0.14, 0.25, 0.4, 0.99);
